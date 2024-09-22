563f8f04cf64ff9eb55e171b752e31fec843444d
lirima11 <araikauali@gmail.com>
2024-09-22 22:42:41 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         TaskManager manager = new TaskManager();
-        Task task1 = new Task(manager.generateId(), "Закупить материалы", "Купить материалы для проекта", Status.NEW);
-        Task task2 = new Task(manager.generateId(), "Завершить отчёт", "Подготовить финансовый отчёт", Status.IN_PROGRESS);
-        manager.addTask(task1);
-        manager.addTask(task2);
 
-        Epic epic1 = new Epic(manager.generateId(), "Переезд", "Переехать в новый офис");
-        Subtask subtask1 = new Subtask(manager.generateId(), "Упаковка вещей", "Упаковать офисные вещи", Status.NEW, epic1);
-        Subtask subtask2 = new Subtask(manager.generateId(), "Перевозка", "Перевезти вещи", Status.NEW, epic1);
-        manager.addEpic(epic1);
-        manager.addSubtask(subtask1);
-        manager.addSubtask(subtask2);
+        Task task1 = manager.createTask("Task 1", "Description 1");
+        Epic epic1 = manager.createEpic("Epic 1", "Description Epic 1");
+        Subtask subtask1 = manager.createSubtask("Subtask 1", "Description Subtask 1", epic1.getId());
+        Subtask subtask2 = manager.createSubtask("Subtask 2", "Description Subtask 2", epic1.getId());
 
-        System.out.println("Все задачи:");
-        for (Task task : manager.getAllTasks()) {
-            System.out.println(task);
-        }
+        System.out.println("Tasks:");
+        System.out.println(manager.getAllTasks());
 
-        subtask1.setStatus(Status.DONE);
-        epic1.updateStatus();
-        System.out.println("Обновлённый эпик: " + epic1);
+        System.out.println("Epics:");
+        System.out.println(manager.getAllEpics());
 
-        manager.deleteTaskById(task1.getId());
-        System.out.println("После удаления задачи:");
-        for (Task task : manager.getAllTasks()) {
-            System.out.println(task);
-        }
+        System.out.println("Subtasks:");
+        System.out.println(manager.getAllSubtasks());
+
+        // Изменение статуса подзадачи
+        subtask1.setStatus(TaskStatus.DONE);
+        manager.updateSubtask(subtask1);
+
+        System.out.println("Updated Epic status:");
+        System.out.println(epic1.getStatus());
     }
 }
