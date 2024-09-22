65ecaa28eedd2abb5137af237a0430310a80d435
lirima11 <araikauali@gmail.com>
2024-09-22 22:44:02 +0500

    Add files via upload

---
+++ src/Subtask.java
@@ -0,0 +1,23 @@
+public class Subtask extends Task {
+    private int epicId;
+
+    public Subtask(String title, String description, int id, int epicId) {
+        super(title, description, id);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "title='" + getTitle() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", id=" + getId() +
+                ", status=" + getStatus() +
+                ", epicId=" + epicId +
+                '}';
+    }
+}
