faae2e56760755a5cbeded861013b0d05fb93d17
Arai <araikauali@gmail.com>
2025-02-06 23:20:29 +0500

    initial comit

---
+++ src/main/InMemoryHistoryManager.java
@@ -0,0 +1,31 @@
+package main;
+
+import manager.HistoryManager;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager {
+    private static final int MAX_HISTORY_SIZE = 10;
+    private final List<Task> history = new LinkedList<>();
+
+
+    @Override
+    public void add(Task task) {
+        if (history.size() == MAX_HISTORY_SIZE) {
+            history.remove(0);
+        }
+        history.add(task);
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return new ArrayList<>(history);
+    }
+}
---
+++ src/main/Managers.java
@@ -0,0 +1,15 @@
+package main;
+
+import manager.HistoryManager;
+import manager.InMemoryTaskManager;
+import manager.TaskManager;
+
+public class Managers {
+    public static TaskManager getDefault() {
+        return new InMemoryTaskManager();
+    }
+
+    public static HistoryManager getDefaultHistory() {
+        return new InMemoryHistoryManager();
+    }
+}
