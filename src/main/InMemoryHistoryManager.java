1c362ffeefb9b68ce475213ca4ac4ee6b7c3051a
Arai <araikauali@gmail.com>
2025-02-16 13:02:14 +0500

    `
    `

---
+++ src/main/InMemoryHistoryManager.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager {
+    private static final int MAX_HISTORY_SIZE = 10;
+    private final List<Task> history = new LinkedList<>();
+
+
+    @Override
+    public void add(Task task) {
+        if (history.size() == MAX_HISTORY_SIZE) {
+            history.remove(0);
+        }
+        history.add(task);
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return new ArrayList<>(history);
+    }
+}
