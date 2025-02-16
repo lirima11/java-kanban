b7c6bae148beb11f494ab105bcb29d7b0a6884d7
Arai <araikauali@gmail.com>
2025-02-16 15:24:38 +0500

    upd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package main;
-
 import manager.TaskManager;
 
 public class Main {
--- src/main/InMemoryHistoryManager.java
+++ src/main/InMemoryHistoryManager.java
@@ -7,19 +7,19 @@ import java.util.List;
 public class InMemoryHistoryManager implements HistoryManager {
     private static final int MAX_HISTORY_SIZE = 10;
     private final List<Task> history = new LinkedList<>();
-
-
+    
     @Override
     public void add(Task task) {
+        history.removeIf(t -> t.getId() == task.getId()); 
         if (history.size() == MAX_HISTORY_SIZE) {
-            history.remove(0);
+            history.remove(0); 
         }
         history.add(task);
     }
-
+    
     @Override
     public void remove(int id) {
-
+        history.removeIf(task -> task.getId() == id);
     }
 
     @Override
